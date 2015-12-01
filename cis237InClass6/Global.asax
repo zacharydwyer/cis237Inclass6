6458792fc4718e4c0a2ef7c1e41eb47a243dc148
David Barnes <barnesdavidj@gmail.com>
2015-12-01 16:36:50 -0500

    Add ASPNET basic framework structure with authentication.

---
+++ cis237InClass6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cis237InClass6.MvcApplication" Language="C#" %>
---
+++ cis237InClass6/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(cis237InClass6.Startup))]
+namespace cis237InClass6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
