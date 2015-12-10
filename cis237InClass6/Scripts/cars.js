30914e261fa823033b7eee1457409f9871ab3f81
David Barnes <barnesdavidj@gmail.com>
2015-12-10 16:43:55 -0500

    Add some JS work.

---
+++ cis237InClass6/Scripts/cars.js
@@ -0,0 +1,10 @@
+﻿//You need this first document ready function almost always. Code goes inside it.
+$(document).ready(function () {
+    //This is a jQuery selector. The '.' is to select a element with the class that
+    //follows the '.', the '#' is to select a element with the id that follows the '#'.
+    $(".dataRow").click(function (firedEvent) {
+        console.log("the event fired");
+        //This line will redirect the browser to this URL.
+        document.location = "/Cars/Edit/0C02938";
+    });
+});
